3bfde9fc8081dbf139a813197841708ed8b29dca
garrip91 <garrip91@gmail.com>
2025-03-01 19:58:18 +0300

    Some more fixes...

--- src/authors_books_project/urls.py
+++ src/authors_books_project/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 
 from .settings import DEBUG
-
-from authors_books_app.views import AuthorListView, GenreViewSet, BookViewSet, AllDataViewSet
 
 
 # ВЕРСИРОВАНИЕ API:
